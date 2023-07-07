ad3f91e8f2d272fe68e3da59a3e10acc22254328
kinahota <138536856+kinahota@users.noreply.github.com>
2023-07-08 00:04:28 +0900

    kadai_021を実施

---
+++ kadai_021/confirmation.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題21</title>
+</head>
+
+<body>
+  <h1>個人情報確認フォーム</h1>
+  <p>お名前：<?php echo $_POST['name'] ?></p>
+  <p>性別：<?php echo $_POST['gender'] ?></p>
+  <p>メールアドレス：<?php echo $_POST['email']; ?></p>
+</body>
+
+</html>
